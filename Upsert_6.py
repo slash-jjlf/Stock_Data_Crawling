a37f09119670186c916f92e9374ccca148fad4af
junilhan77 <junilhan77@gmail.com>
2025-08-15 00:32:11 +0900

    Upsert_6.py의 일부 수정

--- Upsert_6.py
+++ Upsert_6.py
@@ -14,7 +14,7 @@ import numpy as np
 # DB 연결
 engine = create_engine('mysql+pymysql://slash:passion4@192.168.0.53:3306/stock_db')
 con = pymysql.connect(user='slash',
-                      passwd='pasison4',
+                      passwd='passion4',
                       host='192.168.0.53',
                       db='stock_db',
                       charset='utf8')
@@ -23,8 +23,7 @@ mycursor = con.cursor()
 # 티커리스트 불러오기
 ticker_list = pd.read_sql("""
 select * from kor_ticker
-where 기준일 = (select max(기준일) from kor_ticker) and 종목구분 = '보통주'
-and 시가총액 >= 50000000000000;
+where 기준일 = (select max(기준일) from kor_ticker) and 종목구분 = '보통주';
 """, con=engine)
 
 # DB 저장 쿼리
